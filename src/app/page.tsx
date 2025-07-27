3db378ef2caadf713871abf9c2a47d2de3115586
Fazle Rifat Anonto <anonto.kings9@gmail.com>
2025-07-27 13:43:05 +0000

    Make the following changes:
    ,
    
    Find this element:
    
    Relevant files:

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,7 +21,7 @@ const products = [
   {
     title: 'Zenith Shower System',
     description: 'A luxurious shower experience with multiple spray patterns.',
-    image: 'https://placehold.co/600x400',
+    image: 'https://images.unsplash.com/photo-1609280069904-ab36feb3f20c?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHwyMHx8c2hvd2VyJTIwfGVufDB8fHx8MTc1MzYyMzczNXww&ixlib=rb-4.1.0&q=80&w=1080',
     hint: 'shower head',
     gridClass: ''
   },
